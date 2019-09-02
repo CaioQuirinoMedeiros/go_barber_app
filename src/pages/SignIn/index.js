6d73f079e93c19e11574859d0e9c8323bc1cd751
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-02 04:36:18 -0300

    SignUp and SignIn pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
+
+import logo from '../../assets/logo.png';
 
 import Background from '../../components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({navigation}) {
   return (
     <Background>
-      <Text>Sign in</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
+
+import logo from '../../assets/logo.png';
 
 import Background from '../../components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-export default function SignUp() {
+export default function SignUp({navigation}) {
   return (
     <Background>
-      <Text>Sign up</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="E-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Criar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Fazer login</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }

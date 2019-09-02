5e5d65a8582e80f4bb2c259ce82f05c1b7737d91
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-02 02:45:35 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import {View, Text, StatusBar} from 'react-native';
+
+import Routes from './routes';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Routes />
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Background from '../../components/Background';
+
+// import { Container } from './styles';
+
+export default function SignUp() {
+  return (
+    <Background>
+      <Text>Sign up</Text>
+    </Background>
+  );
+}

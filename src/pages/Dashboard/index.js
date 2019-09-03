a6676db40c020e0ef05a89aa38da01d4f26bb86c
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-03 02:11:21 -0300

    Dashshboard page, with appointment component

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState, useCallback} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
 import {useDispatch} from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
 
 import {signOut} from '../../store/modules/auth/actions';
 
 import Background from '../../components/Background';
+import Appointment from '../../components/Appointment';
 
-// import { Container } from './styles';
+import {Container, Title, List} from './styles';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
   const dispatch = useDispatch();
+
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    const response = await api.delete(`appointments/${id}`);
+
+    setAppointments(
+      appointments.map(appointment =>
+        appointment.id === id
+          ? {...appointment, canceled_at: response.data.canceled_at}
+          : {...appointment},
+      ),
+    );
+  }
 
   function logout() {
     dispatch(signOut());
@@ -18,10 +47,19 @@ export default function Dashboard() {
 
   return (
     <Background>
-      <Text>Dashboard</Text>
-      <TouchableOpacity onPress={logout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
+        />
+        <TouchableOpacity onPress={logout}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      </Container>
     </Background>
   );
 }

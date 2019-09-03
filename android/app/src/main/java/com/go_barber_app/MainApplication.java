7f84545652d88f70716830262557cff69b416363
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-03 03:50:26 -0300

    Stack navigator for new appointment pages

--- android/app/src/main/java/com/go_barber_app/MainApplication.java
+++ android/app/src/main/java/com/go_barber_app/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -7,13 +10,38 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
 export default isSigned =>
   createAppContainer(
     createSwitchNavigator(
       {
         Sign: createSwitchNavigator({SignIn, SignUp}),
         App: createBottomTabNavigator(
-          {Dashboard, Profile},
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator({
+                SelectProvider,
+                SelectDateTime,
+                Confirm,
+              }),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
           {
             tabBarOptions: {
               keyboardHidesTabBar: true,

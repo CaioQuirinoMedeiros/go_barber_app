05012880b0af2a55d7c428147671fad42bd44266
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-03 02:11:35 -0300

    adding bearer token in requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+import {store} from '../store';
+
 const api = axios.create({baseURL: 'https://go-barber-api-caio.herokuapp.com'});
+
+api.interceptors.request.use(async config => {
+  const {token} = store.getState().auth;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export default api;

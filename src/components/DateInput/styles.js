c30aab9c19c9b01621988c5dfb9398c96a7983e6
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-09-03 05:50:33 -0300

    Date input in SelectDateTime page

---
+++ src/components/DateInput/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const DateButton = styled.TouchableOpacity`
+  padding: 0 15px;
+  height: 46px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  margin: 0 30px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const DateText = styled.Text`
+  font-size: 14px;
+  color: #fff;
+  margin-left: 15px;
+`;
--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import Background from '../../../components/Background';
+import DateInput from '../../../components/DateInput';
+
+import {Container} from './styles';
 
 export default function SelectDateTime() {
-  return <View />;
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
 }
+
+SelectDateTime.navigationOptions = ({navigation}) => ({
+  title: 'Selecione o horário',
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Icon name="chevron-left" size={22} color="#fff" />
+    </TouchableOpacity>
+  ),
+});

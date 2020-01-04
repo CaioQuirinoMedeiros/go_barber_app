8b27f75c26b23c7a4d68040edc6211f9fefdb6db
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-01-03 23:49:10 -0300

    removing console.log

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,6 @@ import {REACT_APP_API_URL} from 'react-native-dotenv';
 import {store} from '../store';
 
 const api = axios.create({baseURL: REACT_APP_API_URL});
-console.tron.log('REACT_API: ', REACT_APP_API_URL);
 
 api.interceptors.request.use(async config => {
   const {token} = store.getState().auth;
